d524dfa0e89f5a344e0820afee9fb5b669b9798a
Kuwar20 <kuwarx1@gmail.com>
2024-07-17 15:55:03 +0530

    Establish db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,16 +5,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
+// Connect to the database
+import { connectDB } from './utils/connDB.js';
+connectDB();
+
 // Logging
 import bodyParser from 'body-parser';
 import { logRequestResponse } from '../logger.js';
 app.use(bodyParser.json()); // To parse JSON request bodies
 app.use(logRequestResponse);
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
