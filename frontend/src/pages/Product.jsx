7cc14765b28a0dca3f105d185fa82f232d259a1a
Kuwar20 <kuwarx1@gmail.com>
2024-07-22 15:11:40 +0530

    keeping fetched product data in cache so that we can stop repeated loading every time we open the page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,28 +6,36 @@ const Product = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
-            .then(json => {
-                console.log(json);
-                setProductData(json);
-                setLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-                setLoading(false);
-            });
+        const cachedData = localStorage.getItem('productData');
+        
+        if (cachedData) {
+            setProductData(JSON.parse(cachedData));
+            setLoading(false);
+        } else {
+            fetch('https://fakestoreapi.com/products')
+                .then(res => res.json())
+                .then(json => {
+                    console.log(json);
+                    setProductData(json);
+                    localStorage.setItem('productData', JSON.stringify(json));
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.log(err);
+                    setLoading(false);
+                });
+        }
     }, []);
 
     if (loading) return <div>Loading...</div>;
-    if (!productData) return <div>Error loading product data</div>;
+    if (!productData.length) return <div>Error loading product data</div>;
 
     return (
         <div className='min-h-screen'>
             <h1 className='bg-white text-center text-2xl font-bold mb-4 p-4 dark:text-black dark:bg-white'>Product Details</h1>
             <div className='m-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {productData.map((product, index) => (
-                    <Link key={index} to={`/product/${product.id}`} state={{product}} className='block'>
+                    <Link key={product.id} to={`/product/${product.id}`} state={{product}} className='block'>
                         <div className='bg-white shadow-md rounded-lg p-6 w-full h-[450px] flex flex-col hover:shadow-lg transition-transform transform hover:scale-105 cursor-pointer dark:text-black'>
                             <div className='relative overflow-hidden rounded-lg h-[250px]'>
                                 <img
