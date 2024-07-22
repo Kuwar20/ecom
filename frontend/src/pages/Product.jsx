976eb305e4614e61322ad61e1ad01b0f0c283a71
Kuwar20 <kuwarx1@gmail.com>
2024-07-22 15:25:25 +0530

    refactor: Adjust height of product card in Product component
    Update the height of the product card in the Product component to improve the visual presentation. The height is adjusted from 450px to 380px, providing a more balanced and aesthetically pleasing layout. This change enhances the overall user experience by creating a more visually appealing product grid.

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -36,7 +36,7 @@ const Product = () => {
             <div className='m-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
                 {productData.map((product, index) => (
                     <Link key={product.id} to={`/product/${product.id}`} state={{product}} className='block'>
-                        <div className='bg-white shadow-md rounded-lg p-6 w-full h-[450px] flex flex-col hover:shadow-lg transition-transform transform hover:scale-105 cursor-pointer dark:text-black'>
+                        <div className='bg-white shadow-md rounded-lg p-6 w-full h-[380px] flex flex-col hover:shadow-lg transition-transform transform hover:scale-105 cursor-pointer dark:text-black'>
                             <div className='relative overflow-hidden rounded-lg h-[250px]'>
                                 <img
                                     src={product.image}
@@ -45,9 +45,9 @@ const Product = () => {
                                 />
                                 <div className='absolute inset-0 bg-black opacity-0 hover:opacity-40 transition-opacity duration-300'></div>
                             </div>
-                            <div className='flex flex-col items-start mt-4 flex-grow'>
+                            <div className='flex flex-col items-start mt-4'>
                                 <h2 className='text-sm font-semibold mb-2 line-clamp-2'>{product.title}</h2>
-                                <p className='text-sm font-medium text-gray-700 mt-auto'>Price: ${product.price}</p>
+                                <p className='text-sm font-medium text-gray-700 mt-1'>Price: ${product.price}</p>
                             </div>
                         </div>
                     </Link>
